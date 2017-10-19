cebcf726f97d1b07c869a4c1ad6700eecb5b12f5
AndreyS <andreysx@tut.by>
2017-10-19 03:11:38 +0300

    add form registration

---
+++ src/components/Registration.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="registration">
+    <div class="leftForm">
+      <div class="logo"></div>
+      <form>
+        <label for="login">
+          Введите логин</label>
+        <input id="login" type="text" v-model="login" placeholder="Введите логин"><br>
+        <label for="password">Придумайте пароль</label>
+        <input id="password" type="password" v-model="password" placeholder="Придумайте пароль"><br>
+        <label for="repeatPassword">Повторите пароль</label>
+        <input id="repeatPassword" type="password" v-model="repeatPassword" placeholder="Повторите пароль"><br>
+        <label for="phone">Введите телефон</label>
+        <input id="phone" type="text" v-model="phone" placeholder="Введите телефон"><br>
+        <label for="email">Введите Ваш e-mail</label>
+        <input id="email" type="email" v-model="email" placeholder="Введите Ваш e-mail"><br>
+
+        <button @click.prevent="sendingData()" class="btnRegistration">Регистрация</button>
+        <button class="btnBack">У меня есть аккаунт</button>
+      </form>
+      <div class="phone">
+        Единый номер Azur:
+        <span>+375 (29) 123-45-67</span>
+      </div>
+    </div>
+    <div class="rightForm">
+      <div class="welcom">
+        Добро пожаловать в Azur.
+      </div>
+      <div class="infoRegis">Для доступа в Ваш аккаунт, авторизируйтесь.</div>
+      <img src="../assets/big-logo-singin.png" alt="Azur">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'registration',
+  data() {
+    return {
+      login: '',
+      password: '',
+      repeatPassword: '',
+      phone: '',
+      email: ''
+    }
+  },
+  methods: {
+    sendingData: function() {
+      fetch('http://test-task-api.insirion.ru/user/authorization', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          email: this.email,
+          password: this.password,
+          phon: this.phone
+        }
+      }).then((data) => console.log(data))
+    }
+  }
+
+}
+</script>
+
+<style src="../style/registrationForm.css"></style>
